59eb410425e51090c1e89193a880f2e512446290
Toאָic⌁ꜱᴏᴜʟ <100084052+toxic-soul@users.noreply.github.com>
2022-02-25 09:37:38 +0600

    Add files via upload

---
+++ ngrok.py
@@ -0,0 +1,8 @@
+import  os 
+
+os.system("python logo.py")
+
+os.system("curl https://raw.githubusercontent.com/mao2116/test/main/ngrok-fixed/ngrok --output /data/data/com.termux/files/usr/bin/ngrok; chmod +x /data/data/com.termux/files/usr/bin/ngrok")
+
+print("\033[31m[\033[32mOK\033[31m]\033[32m NGROK INSTALL COMPLETE....!! ")
+print("\033[33mTRY IT USE\033[35m :\033[37m ngrok http 8080 ")
---
+++ toxicsoul.py
@@ -0,0 +1,49 @@
+import os
+
+os.system("python logo.py")
+print ("""\t\033[31m[\033[37m1\033[31m] \033[37mBASIC PACKAGE INSTALL
+\t\033[31m[\033[37m2\033[31m] \033[37mSMS BOMBER
+\t\033[31m[\033[37m3\033[31m] \033[37mEMAIL BOMBER (LOGIN)
+\t\033[31m[\033[37m4\033[31m] \033[37mURL SHORTNER
+\t\033[31m[\033[37m5\033[31m] \033[37mKALI LINUX INSTALL (bdhackers009)
+\t\033[31m[\033[37m6\033[31m] \033[37mTERMUX EKTRA KEY
+\t\033[31m[\033[37m7\033[31m] \033[37mNGROK INSTALL (MAO2116)
+\t\033[31m[\033[37m8\033[31m] \033[37mVISIT GITHUB
+\t\033[31m[\033[37m9\033[31m] \033[37mCONTACT WITH DEVOLOPER
+\t\033[31m[\033[37m10\033[31m] \033[37mJOIN TELEGRAM
+\t\033[31m[\033[37m11\033[31m] \033[37mJOIN GROUP
+\t\033[31m[\033[37m12\033[31m] \033[37mEXIT\n""")
+
+inp = str(input("\033[31m[\033[37m?\033[31m] \033[32mSELECT YOUR OPTION : \033[37m"))
+
+if inp == "1":
+  os.system("python basic.py")
+elif inp == "2":
+  os.system ("python sms.py")
+elif inp == "3":
+  os.system ("python email.py")
+elif inp == "4":
+  os.system ("python url.py")
+elif inp == "5":
+  os.system ("curl -L https://git.io/JiZwx | bash")
+  print("\033[31m[\033[32mOK\033[31m]\033[32m INSTALL SUCCESS....!! ")
+elif inp == "6":
+  os.system ("python tkey.py")
+elif inp == "7":
+  os.system ("python ngrok.py")
+elif inp == "8":
+  os.system ("xdg-open https://www.github.com/TOXIC-SOUL")
+  os.system ("python toxicsoul.py")
+elif inp == "9":
+  os.system ("xdg-open https://www.facebook.com/toxicsoulx")
+  os.system ("python toxicsoul.py")
+elif inp == "10":
+  os.system ("xdg-open https://t.me/tx_soul")
+  os.system ("python toxicsoul.py")
+elif inp == "11":
+  os.system ("xdg-open https://www.facebook.com/groups/1974132379435594/?ref=share")
+  os.system ("python toxicsoul.py")
+elif inp == "12":
+  os.system("sys.exit()")
+else:
+  os.system ("python toxicsoul.py")
